aa098a4e1e52adb798b56e56904994d4c31112af
Mark Basford <mark.basford@gmail.com>
2019-07-30 13:28:07 -0500

    Add dinner button and data arrays on first screen

--- Table View Test/SelectionViewController.swift
+++ Table View Test/SelectionViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class SelectionViewController: UIViewController {
 
-    var lunchArray = ["dice1", "dice5", "Hello World  Hello World Hello World Hello World Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World Hello World"]
+   // var lunchArray = ["dice1", "dice5", "Hello World  Hello World Hello World Hello World Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World", "Go Gators", "Hello World Hello World"]
+    
+    let lunchArrayOne = ["Viet Nom Nom", "dice1", "10% Off Bahn Mi"]
+    let lunchArrayTwo = ["Bat 17", "dice5", "Half Price Sandwiches"]
+    let lunchArrayThree = ["Taco Diablo", "dice1", "20% Off Tacos"]
+    
+    let dinnerArrayOne = ["Giordanos", "dice5", "25% Off deep dish"]
+    let dinnerArrayTwo = ["Cozy Noodles", "dice1", "$10 Noodle Dishes"]
+    let dinnerArrayThree = ["Little Mexican Cafe", "dice5", "Half Price Tacos"]
+    
+    
+    @IBOutlet weak var lunchButton: UIButton!
+    
+    @IBOutlet weak var dinnerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        lunchButton.layer.cornerRadius = 10
+        dinnerButton.layer.cornerRadius = 10
         
     }
     
@@ -21,8 +37,54 @@ class SelectionViewController: UIViewController {
         
         performSegue(withIdentifier: "goToTableView", sender: self)
         
+    }
+    
+    @IBAction func dinnerButtonPressed(_ sender: Any) {
+        
+        performSegue(withIdentifier: "dinnerSegue", sender: self)
         
     }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToTableView" {
+            
+            let destinationVC = segue.destination as! TableViewController
+            
+            destinationVC.restaurantName1 = lunchArrayOne[0]
+            destinationVC.restaurantName2 = lunchArrayTwo[0]
+            destinationVC.restaurantName3 = lunchArrayThree[0]
+            
+            destinationVC.picture1 = lunchArrayOne[1]
+            destinationVC.picture2 = lunchArrayTwo[1]
+            destinationVC.picture3 = lunchArrayThree[1]
+         
+            destinationVC.text1 = lunchArrayOne[2]
+            destinationVC.text2 = lunchArrayTwo[2]
+            destinationVC.text3 = lunchArrayThree[2]
+
+            
+        }
+        if segue.identifier == "dinnerSegue" {
+            
+            let destinationVC = segue.destination as! TableViewController
+            
+            destinationVC.restaurantName1 = dinnerArrayOne[0]
+            destinationVC.restaurantName2 = dinnerArrayTwo[0]
+            destinationVC.restaurantName3 = dinnerArrayThree[0]
+            
+            destinationVC.picture1 = dinnerArrayOne[1]
+            destinationVC.picture2 = dinnerArrayTwo[1]
+            destinationVC.picture3 = dinnerArrayThree[1]
+
+            destinationVC.text1 = dinnerArrayOne[2]
+            destinationVC.text2 = dinnerArrayTwo[2]
+            destinationVC.text3 = dinnerArrayThree[2]
+            
+        }
+        
+    }
+    
     
    // override func prepare(for segue: UIStoryboardSegue, sender: (Any)?) {
      // if segue.identifier == "goToTableView" {
--- Table View Test/TableViewController.swift
+++ Table View Test/TableViewController.swift
@@ -18,22 +18,21 @@ class TableViewController: UITableViewController {
     let dayOfWeek = 1
     
     // Restaurant arrays contain promotion information and are organized based on day of the week (0 = Monday, 1 = Tuesday, etc.).  "nil" means there is no deal that day.
-    let restaurantName1 = "VietNomNom Nom Nom Nom Nom"
-    let restaurantName2 = "Blaze Pizza"
-    let restaurantName3 = "Five & Dime"
+    var restaurantName1 = ""
+    var restaurantName2 = ""
+    var restaurantName3 = ""
     
+    //var restaurant1 = ["",""]
+    //var restaurant2 = ""
+    //var restaurant3 = ""
     
-    let restaurant1 = ["dice1","dice1"]
-    let restaurant2 = "dice5"
-    let restaurant3 = "Hello World"
-    
-    let picture1 = "dice1"
-    let picture2 = "dice5"
-    let picture3 = "dice1"
+    var picture1 = ""
+    var picture2 = ""
+    var picture3 = ""
 
-    let text1 = "Hello World"
-    let text2 = "Go Gators"
-    let text3 = "Go Jags"
+    var text1 = ""
+    var text2 = ""
+    var text3 = ""
     
     
 // TABLE VIEW DATA:
@@ -43,9 +42,9 @@ class TableViewController: UITableViewController {
         return [self.restaurantName1, self.restaurantName2, self.restaurantName3]
         }()
     
-    lazy var itemArray: [String] = { [unowned self] in
-        return [self.restaurant1[self.dayOfWeek], self.restaurant2, self.restaurant3]
-    }()
+//    lazy var itemArray: [String] = { [unowned self] in
+//        return [self.restaurant1[self.dayOfWeek], self.restaurant2, self.restaurant3]
+//    }()
     
     lazy var pictureArray: [String] = {[unowned self] in return [self.picture1, self.picture2, self.picture3] }()
     
@@ -61,18 +60,18 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return restaurantNameArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for:  indexPath) as! CustomMessageCell
         
-        cell.messageBody.text = itemArray[indexPath.row]
+        cell.messageBody.text = textArray[indexPath.row]
         cell.avatarImageView.image = UIImage(named: pictureArray[indexPath.row])
         cell.promotionDescription.text = restaurantNameArray[indexPath.row]
         
-        if let color = UIColor.flatWhite()?.darken(byPercentage: (CGFloat(indexPath.row)/CGFloat(itemArray.count))/2) {
+        if let color = UIColor.flatWhite()?.darken(byPercentage: (CGFloat(indexPath.row)/CGFloat(restaurantNameArray.count))/2) {
             
             cell.backgroundColor = color
             cell.messageBody.textColor = UIColor.init(contrastingBlackOrWhiteColorOn: color, isFlat: true)
